89e2cacf250a51616e5a88ad2e50f5cf244a0d1b
Michael Souza <contact@michael-souza.com>
2016-02-07 10:02:27 -0500

    Added CSS purification support

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 var uglify = require('gulp-uglify');
+var minifycss = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var purifycss = require('gulp-purifycss');
 var sassdoc = require('sassdoc');
 var exec = require('child_process').exec;
 
@@ -12,9 +14,10 @@ var paths = {
   jquery: 'node_modules/jquery/dist',
   assets: '_assets',
   sass: '_assets/stylesheets',
+  site: '_site',
   css: 'assets/css',
   fonts: 'assets/fonts',
-  js: 'assets/js'
+  js: 'assets/js',
 };
 
 var sassOptions = {
@@ -27,11 +30,19 @@ var autoprefixerOptions = {
   browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
 };
 
+//noinspection JSUnusedGlobalSymbols
+var sourcemapsOptions = {
+  mapFile: function (mapFilePath) {
+    // source map files are named *.map instead of *.js.map
+    return mapFilePath.replace('.js.map', '.map');
+  }
+};
+
 /*
  * Update Tasks
  */
 gulp.task('update-jquery', function(cb) {
-  exec('npm update jquery', function(err, stdout, stderr) {
+  return exec('npm update jquery', function(err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     cb(err);
@@ -45,7 +56,7 @@ gulp.task('copy-jquery', function() {
 });
 
 gulp.task('update-bootstrap', function(cb) {
-  exec('npm update bootstrap-sass', function(err, stdout, stderr) {
+  return exec('npm update bootstrap-sass', function(err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     cb(err);
@@ -94,18 +105,33 @@ gulp.task('build-sassdoc', function() {
 });
 
 gulp.task('compress-js', function () {
-  gulp.src(paths.js + '/custom.js')
+  return gulp
+    .src(paths.js + '/custom.js')
     .pipe(sourcemaps.init())
       .pipe(uglify())
       .pipe(rename({
         suffix: '.min'
       }))
-    .pipe(sourcemaps.write('./'))
+    .pipe(sourcemaps.write('.', sourcemapsOptions))
     .pipe(gulp.dest(paths.js));
 });
 
-gulp.task('jekyll-build', function (cb) {
-  exec('jekyll build', function(err, stdout, stderr) {
+gulp.task('compress-css', function() {
+  return gulp
+    .src([paths.css + '/*.css', '!' + paths.css + '/*.min.css'])
+    .pipe(sourcemaps.init())
+      .pipe(minifycss())
+      .pipe(rename({
+        suffix: '.min'
+      }))
+    .pipe(sourcemaps.write('.', sourcemapsOptions))
+    .pipe(gulp.dest(paths.css));
+});
+
+gulp.task('compress', ['compress-js', 'compress-css']);
+
+gulp.task('jekyll-build', ['build-bootstrap'], function (cb) {
+  return exec('jekyll build', function(err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     cb(err);
@@ -115,28 +141,36 @@ gulp.task('jekyll-build', function (cb) {
 gulp.task('build-bootstrap', function () {
   return gulp
     .src(paths.sass + '/custom.scss')
-    //.pipe(sourcemaps.init())
-    .pipe(sass(sassOptions))
-    //.pipe(sourcemaps.write())
-    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(sourcemaps.init())
+      .pipe(sass(sassOptions))
+      .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(sourcemaps.write('.', sourcemapsOptions))
     .pipe(gulp.dest(paths.css));
 });
 
-gulp.task('build', ['build-bootstrap', 'compress-js', 'jekyll-build']);
+gulp.task('purify', ['jekyll-build'], function() {
+  return gulp.src([paths.css + '/custom.css'])
+    .pipe(sourcemaps.init())
+      .pipe(purifycss([paths.js + '/custom.js', paths.site + '/**/*.html']))
+    .pipe(sourcemaps.write('.', sourcemapsOptions))
+    .pipe(gulp.dest(paths.css));
+});
+
+gulp.task('build', ['purify', 'compress-js']);
 
 /*
  * Testing and Debugging Tasks
  */
 gulp.task('jekyll-serve', function (cb) {
-  exec('jekyll serve', function(err, stdout, stderr) {
+  return exec('jekyll serve', function(err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     cb(err);
   });
 });
 
-gulp.task('watch', function () {
-  gulp.watch(paths.sass + '/*.scss', ['build-bootstrap']);
+gulp.task('watch', ['build-bootstrap'], function () {
+  return gulp.watch(paths.sass + '/*.scss', ['build-bootstrap']);
 });
 
 gulp.task('debug', ['watch', 'jekyll-serve']);
